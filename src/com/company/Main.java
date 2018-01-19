07769bcd2460879f1eb9e798d3454351c050fcae
Ray3 <ray3@github.com>
2018-01-19 10:30:04 -0600

    1234

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // Welcome Message
-        System.out.println("\nWelcome");
+        // Record user's name
+        Scanner sc = new Scanner(System.in);
+        System.out.print("\nEnter your Name: ");
+        String name = sc.nextLine();
+        System.out.println();
+
+        // Output Welcome Message
+        System.out.println("Welcome " + name);
     }
 }
